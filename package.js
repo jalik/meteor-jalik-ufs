49acc6cc3a83ca7e9ad0e0fe490a89b76d53dd7b
Karl STEIN <karl.stein.pro@gmail.com>
2015-08-19 11:57:01 -1000

    version 0.2.6

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'jalik:ufs',
-    version: '0.2.5',
+    version: '0.2.6',
     author: 'karl.stein.pro@gmail.com',
     summary: 'Base package for UploadFS',
     homepage: 'https://github.com/jalik/jalik-ufs',
